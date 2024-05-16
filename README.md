5f055c10c3ce253b4f3524018affd69c32a01a84
Gihozo23 <christellegihozo23@gmail.com>
2024-05-16 19:46:54 +0200

    javascript exervises

--- README.md
+++
@@ -1,1 +0,0 @@
-# web-cloning-exercise
---
+++ exercises.js
@@ -0,0 +1,5 @@
+let par = document.createElement("p");
+let text=document.createTextNode("the third paragraph");
+par.id="p3";
+let division =getElementById('div1');
+division.appendchild(par);
